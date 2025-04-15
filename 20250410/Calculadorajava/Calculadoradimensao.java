7cc3600b27d747fae8320442fa91204528286552
Atila <thalitinha246@gmail.com>
2025-04-15 12:00:11 -0300

    calculadora

--- 20250410/Calculadorajava/Calculadoradimensao.java
+++ 20250410/Calculadorajava/Calculadoradimensao.java
@@ -1,42 +1,42 @@
 import java.util.Random;
 
-public class JogoDaVelha {
+public class Calculadoradimensao {
     protected static final int X = 1, O = -1;
     protected static final int VAZIO = 0;
     protected int tabuleiro[][];
     protected int jogador;
-    protected int d;
+    protected int dms;
 
-    public JogoDaVelha(int dimensao) {
+    public Calculadoradimensao(int dimensao) {
         limpaTabuleiro();
         this.tabuleiro = new int[dimensao][dimensao];
-        this.d = dimensao;
+        this.dms = dimensao;
     };
 
     public void limpaTabuleiro() {
-        for (int i = 0; i < d; i++) {
-            for (int j = 0; j < d; j++) {
-                tabuleiro[i][j] = VAZIO;
+        for (int l = 0; l < dms; l++) {
+            for (int c = 0; c < dms; c++) {
+                tabuleiro[l][c] = VAZIO;
             }
         }
         jogador = X;
     };
 
-    public void poePeca(int i, int j) {
-        if (i < 0 || i > 4 || j < 0 || j > 4) {
-            throw new IllegalArgumentException("Posição inválida");
+    public void poePeca(int l, int c) {
+        if (l < 0 || l > 4 || c < 0 || c > 4) {
+            throw new IllegalArgumentException("Esta posição é inválida para por peça");
         }
-        if (tabuleiro[i][j] != VAZIO)
-            throw new IllegalArgumentException("Posição ocupada");
-        tabuleiro[i][j] = jogador;
+        if (tabuleiro[l][c] != VAZIO)
+            throw new IllegalArgumentException("Esta posição está ocupada");
+        tabuleiro[l][c] = jogador;
 
         jogador = -jogador;
     }
 
     public int[] gerarLinhaColuna() {
         Random gerador = new Random();
-        int linha = gerador.nextInt(0, d);
-        int coluna = gerador.nextInt(0, d);
+        int linha = gerador.nextInt(0, dms);
+        int coluna = gerador.nextInt(0, dms);
         return new int[] { linha, coluna };
     }
 
@@ -63,37 +63,37 @@ public class JogoDaVelha {
     public boolean descVencedor(int marca) {
         int somaDiagonal1 = 0;
         int somaDiagonal2 = 0;
-        for (int i = 0; i < d; i++) {
+        for (int l = 0; l < dms; l++) {
             int somaLinha = 0;
-            for (int j = 0; j < d; j++) {
-                somaLinha += tabuleiro[i][j];
-                if (i == j) {
-                    somaDiagonal1 += tabuleiro[i][j];
+            for (int c = 0; c < dms; c++) {
+                somaLinha += tabuleiro[l][c];
+                if (l == c) {
+                    somaDiagonal1 += tabuleiro[l][c];
                 }
 
-                if (i + j == 2) {
-                    somaDiagonal2 += tabuleiro[i][j];
+                if (l + c == 2) {
+                    somaDiagonal2 += tabuleiro[l][c];
                 }
             }
-            if (somaLinha == marca * d) {
+            if (somaLinha == marca * dms) {
                 return true;
             }
         }
 
-        if (somaDiagonal1 == marca * d) {
+        if (somaDiagonal1 == marca * dms) {
             return true;
         }
 
-        if (somaDiagonal2 == marca * d) {
+        if (somaDiagonal2 == marca * dms) {
             return true;
         }
 
-        for (int j = 0; j < d; j++) {
+        for (int c = 0; c < dms; c++) {
             int somaColuna = 0;
-            for (int i = 0; i < d; i++) {
-                somaColuna += tabuleiro[i][j];
+            for (int l = 0; l < dms; l++) {
+                somaColuna += tabuleiro[l][c];
             }
-            if (somaColuna == marca * d) {
+            if (somaColuna == marca * dms) {
                 return true;
             }
         }
@@ -107,9 +107,9 @@ public class JogoDaVelha {
         } else if (descVencedor(O)) {
             return O;
         } else {
-            for (int i = 0; i < d; i++) {
-                for (int j = 0; j < d; j++) {
-                    if (tabuleiro[i][j] == VAZIO) {
+            for (int l = 0; l < dms; l++) {
+                for (int c = 0; c < dms; c++) {
+                    if (tabuleiro[l][c] == VAZIO) {
                         return 2;
                     }
                 }
@@ -119,29 +119,29 @@ public class JogoDaVelha {
     }
 
     public String toString() {
-        String s = "";
-        for (int i = 0; i < d; i++) {
-            for (int j = 0; j < d; j++) {
-                switch (tabuleiro[i][j]) {
+        String j = "";
+        for (int l = 0; l < dms; l++) {
+            for (int c = 0; c < dms; c++) {
+                switch (tabuleiro[l][c]) {
                     case X:
-                        s += "X";
+                        j += "X";
                         break;
                     case O:
-                        s += "O";
+                        j += "O";
                         break;
                     case VAZIO:
-                        s += " ";
+                        j += " ";
                         break;
                 }
-                if (j < d - 1)
-                    s += "|"; // limite da coluna
+                if (c < dms - 1)
+                    j += "|"; // limite da coluna
             }
-            if (i < d - 1) {
-                String tracos = "-".repeat(d * 2 - 1); // limite da linha
-                s += "\n" + tracos + "\n"; // limite da linha
+            if (l < dms - 1) {
+                String tracos = "-".repeat(dms * 2 - 1); // limite da linha
+                j += "\n" + tracos + "\n"; // limite da linha
             }
         }
-        return s;
+        return j;
     }
 
     public int getJogador() {
--- 20250410/Calculadorajava/Jogando.java
+++ 20250410/Calculadorajava/Jogando.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class App {
+public class Jogando {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        boolean jogarNovamente = true;
+        boolean jogarAgain = true;
         
-        while (jogarNovamente) {
+        while (jogarAgain) {
             System.out.println("Bem-vindo ao Jogo da Velha!");
             System.out.println("Escolha o tamanho do tabuleiro: ");
             int dimensao = sc.nextInt();
@@ -13,17 +13,17 @@ public class App {
                 System.out.println("O tamanho do tabuleiro deve ser impar. Tente novamente.");
                 continue;
             }
-            JogoDaVelha jogo2 = new JogoDaVelha(dimensao);
-            int acabou = jogo2.poePecaAutomatico();
+            Calculadoradimensao jogonovo = new Calculadoradimensao(dimensao);
+            int acabou = jogonovo.poePecaAutomatico();
             
             while (acabou == 2) {
-                System.out.println(jogo2.toString());
-                System.out.println("Vez do jogador " + (jogo2.getJogador() == 1 ? "X" : "O"));
+                System.out.println(jogonovo.toString());
+                System.out.println("Vez do jogador " + (jogonovo.getJogador() == 1 ? "X" : "O"));
                 System.out.println("\n");
-                acabou = jogo2.poePecaAutomatico();
+                acabou = jogonovo.poePecaAutomatico();
             }
             
-            System.out.println(jogo2.toString());
+            System.out.println(jogonovo.toString());
             
             if (acabou == 1) {
                 System.out.println("Jogador X venceu!");
@@ -47,7 +47,7 @@ public class App {
                 }
             } while (resposta != 0 && resposta != 1);
             
-            jogarNovamente = (resposta == 1);
+            jogarAgain = (resposta == 1);
         }
         
         System.out.println("Obrigado por jogar!");
